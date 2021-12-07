21e6e5b0b51cb98f9512957a750a78f489450a23
GGilookSeagull <92835879+GGilookSeagull@users.noreply.github.com>
2021-12-07 10:28:24 +0900

    Update camera.py

--- Refrigerator_crash/camera.py
+++ Refrigerator_crash/camera.py
@@ -7,7 +7,7 @@ from time import sleep
 
 camera = PiCamera()
 
-# '3'초간 녹화를 하겠다는 클래스를 Rec()라고 부르기로 했어요
+# '3'초간 녹화를 하겠다는 함수를 Rec()라고 부르기로 했어요
 def Rec():
     # 대충 녹화 시작하고 3초 녹화하다 끝내고 camera.h264로 저장하겠다는 내용
     camera.start_preview()

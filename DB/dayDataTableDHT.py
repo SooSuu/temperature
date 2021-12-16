03e1a68463fb2f256104d4958a27dcd3dbea6943
sososoyu <95513427+sososoyu@users.noreply.github.com>
2021-12-16 14:46:04 +0900

    Add files via upload

---
+++ DB/dayDataTableDHT.py
@@ -0,0 +1,10 @@
+import sqlite3 as lite
+from datetime import date
+import sys
+con = lite.connect('sensorsData.db')
+with con:
+    cur =con.cursor()
+    cur.execute("DROP TABLE IF EXISTS Shelf_life")
+    cur.execute("CREATE TABLE Shelf_life(timestamp DATE,name TEXT ,shelf DATE)")
+    cur.execute("DROP TABLE IF EXISTS Shelf_made")
+    cur.execute("CREATE TABLE Shelf_made(timestamp DATE,name TEXT ,shelf DATE)")
